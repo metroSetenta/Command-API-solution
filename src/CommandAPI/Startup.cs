ee9534aea9f72b31eac6a91ec83950889090aa8d
Javier Guerrero <metrosetenta@gmail.com>
2021-12-08 08:10:44 +0100

    Used .NET configuration layer to hide sensitive items

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
+using Npgsql;
 
 namespace CommandAPI
 {
@@ -22,8 +23,12 @@ namespace CommandAPI
     }
     public void ConfigureServices(IServiceCollection services)
     {
-      services.AddDbContext<CommandContext>(opt => opt.UseNpgsql
-      (Configuration.GetConnectionString("PostgreSqlConnection")));
+      var builder = new NpgsqlConnectionStringBuilder();
+      builder.ConnectionString = Configuration.GetConnectionString("PostgreSqlConnection");
+      builder.Username = Configuration["UserID"];
+      builder.Password = Configuration["Password"];
+
+      services.AddDbContext<CommandContext>(opt => opt.UseNpgsql(builder.ConnectionString));
 
       services.AddControllers();
 
